f72758ff7a98b1eacc984e178d3b41fe875a8a10
nabingx <nngocanh697@gmail.com>
2022-04-09 14:28:54 +0700

    Update

---
+++ Lab02/DisplayDayofMonth.java
@@ -0,0 +1,105 @@
+package Lab02;
+
+//import java.util.Arrays;
+import java.util.Scanner;
+
+public class DisplayDayofMonth {
+	
+	public static boolean isLeapYear(int year) {
+		if (year % 4 != 0) {
+			return false;
+		} else if (year % 400 == 0) {
+		    return true;
+		} else if (year % 100 == 0) {
+		    return false;
+		} else {
+		    return true;
+		}
+	}
+	
+	public static void DaysInCommonYear(String month) {
+		int Day = 0;
+		switch(month){
+		case "1": Day = 31;
+		case "2": Day = 28;
+		case "3": Day = 31;
+		case "4": Day = 30;
+		case "5": Day = 31;
+		case "6": Day = 30;
+		case "7": Day = 31;
+		case "8": Day = 31;
+		case "9": Day = 30;
+		case "10": Day = 31;
+		case "11": Day = 30;
+		case "12": Day = 31;
+		}
+		System.out.println("Days of the month: " + Day);
+	}
+	
+	public static void DaysInLeapYear(String month) {
+		int Day = 0;
+		switch(month){
+		case "1": Day = 31;
+		case "2": Day = 29;
+		case "3": Day = 31;
+		case "4": Day = 30;
+		case "5": Day = 31;
+		case "6": Day = 30;
+		case "7": Day = 31;
+		case "8": Day = 31;
+		case "9": Day = 30;
+		case "10": Day = 31;
+		case "11": Day = 30;
+		case "12": Day = 31;
+		}
+		System.out.println("Days of the month: " + Day);
+	}
+	
+	public static void main(String[] args) {
+		
+		InputMonth[] Month = new InputMonth[12];
+		
+		Month[0] = new InputMonth("January", "Jan.", "Jan", "1");
+		Month[1] = new InputMonth("February", "Feb.", "Feb", "2");
+		Month[2] = new InputMonth("March", "Mar.", "Mar", "3");
+		Month[3] = new InputMonth("April", "Apr.", "Apr", "4");
+		Month[4] = new InputMonth("May", "May", "May", "5");
+		Month[5] = new InputMonth("June", "June", "June", "6");
+		Month[6] = new InputMonth("July", "July", "July", "7");
+		Month[7] = new InputMonth("August", "Aug.", "Aug", "8");
+		Month[8] = new InputMonth("September", "Sep.", "Sep", "9");
+		Month[9] = new InputMonth("October", "Oct.", "Oct", "10");
+		Month[10] = new InputMonth("November", "Nov.", "Nov", "11");
+		Month[11] = new InputMonth("December", "Dec.", "Dec", "12");
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a month: ");
+		String value = keyboard.next();
+		String answer = "";
+		
+		for(int i = 0; i < 12; i++) {
+			if(value.equals(Month[i].month) || value.equals(Month[i].abbre) || value.equals(Month[i].in3Letters) || value.equals(Month[i].inNumber)) {
+				System.out.println("Month: " + Month[i].month);
+				System.out.println("Abbreviation: " + Month[i].abbre);
+				System.out.println("In 3 letters: " + Month[i].in3Letters);
+				System.out.println("In Number: " + Month[i].inNumber);
+				answer = Month[i].inNumber;
+				}
+		}
+		
+		System.out.println("Enter a year: ");
+		int year = keyboard.nextInt();
+		while(year != (int)year || year < 1000){	// Em đặt điều kiện < 1000 vì không rõ chỗ đề bài giải thích year không được bằng 99
+			System.out.println("Wrong input, enter again: ");
+			year = keyboard.nextInt();
+		}
+		
+		System.out.println("Year: " + year);
+		if(isLeapYear(year)) DaysInLeapYear(answer);
+		else DaysInCommonYear(answer);
+		
+		keyboard.close();
+
+	}
+
+}
---
+++ Lab02/InputMonth.java
@@ -0,0 +1,14 @@
+package Lab02;
+
+public class InputMonth {
+	
+	String month, abbre, in3Letters, inNumber;
+	
+	InputMonth(String month, String abbre, String in3Letters, String inNumber) {
+		this.month = month;
+		this.abbre = abbre;
+		this.in3Letters = in3Letters;
+		this.inNumber = inNumber;
+	}
+	
+}
