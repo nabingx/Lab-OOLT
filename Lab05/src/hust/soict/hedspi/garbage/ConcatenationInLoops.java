6d5fc5094952569d83716dc79f5d45e28a20513d
nabingx <nngocanh697@gmail.com>
2022-05-15 10:56:51 +0700

    Update

--- Lab05/src/hust/soict/hedspi/garbage/ConcatenationInLoops.java
+++ Lab05/src/hust/soict/hedspi/garbage/ConcatenationInLoops.java
@@ -20,7 +20,20 @@ public class ConcatenationInLoops {
 			sb.append(r.nextInt(2));
 		s = sb.toString();
 		System.out.println(System.currentTimeMillis() - start); // This prints 5
-
+		
+		r = new Random(123);
+		start = System.currentTimeMillis();
+		StringBuffer sb2 = new StringBuffer();
+		for(int i = 0; i < 65536; i++)
+			sb2.append(r.nextInt(2));
+		s = sb2.toString();
+		System.out.println(System.currentTimeMillis() - start);
+		
+		new GarbageCreator();
+		//new NoGarbage();
+		// Dùng 1 trong 2 để ktra
+		// Use 1 only to check
+		
 	}
 
 }
