c170602cc9b34d17c9e87030775832513e421089
nabingx <nngocanh697@gmail.com>
2022-05-26 18:15:25 +0700

    Update

--- Lab07/src/hust/soict/hedspi/aims/Aims.java
+++ Lab07/src/hust/soict/hedspi/aims/Aims.java
@@ -2,9 +2,13 @@ package Lab07.src.hust.soict.hedspi.aims;
 
 import java.util.Scanner;
 
-import Lab06.src.hust.soict.hedspi.aims.order.Order;
-import Lab06.src.hust.soict.hedspi.media.Media;
-import Lab06.src.hust.soict.hedspi.test.disc.TestPassingParameter;
+import Lab07.src.hust.soict.hedspi.aims.disc.CompactDisc;
+import Lab07.src.hust.soict.hedspi.aims.order.Order;
+import Lab07.src.hust.soict.hedspi.media.Media;
+import Lab07.src.hust.soict.hedspi.test.disc.TestPassingParameter;
+import Lab07.src.hust.soict.hedspi.media.Book;
+
+// Nhớ chọn 1 khi menu lần đầu show ra để khởi tạo anOrder
 
 public class Aims extends TestPassingParameter{
 	
@@ -15,8 +19,9 @@ public class Aims extends TestPassingParameter{
 		//int counter = 0;
 		Order anOrder = null;
 		int id = 0;
+		int c = 0;
 		
-		while(choice < 5) {
+		while(choice < 6) {
 			System.out.print("Enter a number: ");
 			Scanner sc = new Scanner(System.in);
 			choice = sc.nextInt();
@@ -27,12 +32,15 @@ public class Aims extends TestPassingParameter{
 			}
 			case 1:
 				anOrder = new Order();
+				System.out.println("Initialize anOrder successfully!");
 				break;
-			case 2:
+			/*case 2:
 				System.out.print("New Item's Title: ");
 				newTitle = sc.next();
-				anOrder.addMedia(new Media(newTitle));
-				break;
+				//anOrder.addMedia(new Media(newTitle));
+				Media media = new Media(newTitle);
+				anOrder.addMedia();
+				break;*/
 			case 3:
 				System.out.println("Enter id(number): ");
 				id = sc.nextInt();
@@ -44,9 +52,40 @@ public class Aims extends TestPassingParameter{
 					System.out.println( (i + 1) + ". " + anOrder.getItemsOrdered2(i));
 				}
 				break;
+			case 2:
+				System.out.print("Choose a number (Book - 1, CD - 2, DVD - 3): ");
+				c = sc.nextInt();
+				switch (c) {
+				case 1:
+					System.out.println("New Book's Title: ");
+					newTitle = sc.next();
+					anOrder.addMedia(new Book(newTitle));
+					break;
+				case 2:
+					System.out.println("New CD's Title: ");
+					newTitle = sc.next();
+					CompactDisc cd = new CompactDisc(newTitle);
+					anOrder.addMedia(cd);
+					System.out.println("Do you want to play the CD(yes/no)?");
+					if(sc.next().equals("yes")) cd.play();
+					break;
+				case 3:
+					System.out.println("New DVD's Title: ");
+					newTitle = sc.next();
+					CompactDisc dvd = new CompactDisc(newTitle);
+					anOrder.addMedia(dvd);
+					System.out.println("Do you want to play the DVD(yes/no)?");
+					String input = sc.next();
+					if(input.equals("yes")) dvd.play();
+					break;
+				default:
+					System.out.println("Wrong number of disc");
+					break;
+				}
 			default:
 				System.out.println("Wrong number. Please try again");
-			}
+				break; // Em ko hiểu sao sau khi xong case 2 của choice nó cứ in cái default này ạ
+				}
 		}
 	}
 	
@@ -57,6 +96,7 @@ public class Aims extends TestPassingParameter{
 		System.out.println("2. Add item to the order");
 		System.out.println("3. Delete item by id");
 		System.out.println("4. Display the items list of order");
+		//System.out.println("5. Add Book, CD or DVD to the order");
 		System.out.println("0. Exit");
 		System.out.println("--------------------------------");
 		System.out.println("Please choose a number: 0-1-2-3-4");
